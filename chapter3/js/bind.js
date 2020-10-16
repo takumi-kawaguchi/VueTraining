833e14a242a9b18450c136fed5248e7320906a85
Takumi Kawaguchi <tcgds4@gmail.com>
2020-10-16 23:54:25 +0900

    chap3-20201016

--- chapter3/js/bind.js
+++ chapter3/js/bind.js
@@ -1,3 +1,13 @@
 new Vue({
-
+    el: '#app',
+    data: {
+        attr: 'width',
+        size: 100,
+        attrs: {
+            size: 20,
+            maxlength: 14,
+            required: true
+        },
+        text: 'hello, world!'
+    }
 });
---
+++ chapter3/js/html.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: '<img src="https://wings.msn.to/image/wings.jpg">',
+        name: 'Alice'
+    }
+});
