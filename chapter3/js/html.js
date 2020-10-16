c39b49b9286538ff4833ae07a0a49561cae4e125
Takumi Kawaguchi <tcgds4@gmail.com>
2020-10-17 00:04:30 +0900

    chap3-20201017

--- chapter3/js/html.js
+++ chapter3/js/html.js
@@ -3,5 +3,13 @@ new Vue({
     data: {
         message: '<img src="https://wings.msn.to/image/wings.jpg">',
         name: 'Alice'
+    },
+    methods: {
+        onsubmit: function(e) {
+            if (!confirm('送信しても良いですか？')) {
+                e.preventDefault();
+                return;
+            }
+        }
     }
 });
